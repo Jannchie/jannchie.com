f66a833ff285532e4e2b7afdaaa439293ff6efce
Jannchie <jannchie@gmail.com>
2023-07-10 01:15:52 +0900

    :adhesive_bandage: fix: pwa style

--- components/ReloadPrompt.vue
+++ components/ReloadPrompt.vue
@@ -43,18 +43,14 @@ async function close() {
   bottom: 0;
   margin: 16px;
   padding: 12px;
-  border: 1px solid #8885;
   border-radius: 4px;
   z-index: 1;
   text-align: left;
-  box-shadow: 3px 4px 5px 0 #8885;
-  background-color: white;
 }
 .pwa-toast .message {
   margin-bottom: 8px;
 }
 .pwa-toast button {
-  border: 1px solid #8885;
   outline: none;
   margin-right: 5px;
   border-radius: 2px;
