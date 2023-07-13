99545f7bdb44839dad2527d91919576d9c5c4e52
Jannchie <jannchie@gmail.com>
2023-07-13 23:56:07 +0900

    :sparkles: feat: content title style

---
+++ components/content/ProseH3.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useRuntimeConfig } from '#imports'
+
+defineProps<{ id?: string }>()
+const heading = 3
+const { anchorLinks } = useRuntimeConfig().public.content
+const generate = anchorLinks?.depth >= heading && !anchorLinks?.exclude.includes(heading)
+</script>
+
+<template>
+  <h2 :id="id" class="relative">
+    <a v-if="id && generate" data-cursor="block" class="hover:before:content-['###'] before:-left-[4ch] before:absolute before:text-fg-3 decoration-offset-8 font-bold" :href="`#${id}`">
+      <slot />
+    </a>
+    <slot v-else />
+  </h2>
+</template>
