93218986dde4b0af896d4d142a15440487e82b03
Jannchie <jannchie@gmail.com>
2023-07-24 23:13:31 +0900

    :wrench: refactor: use article page component

---
+++ components/ArticlePage.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import 'katex/dist/katex.min.css'
+
+const { title } = defineProps<{
+  title: string
+}>()
+
+const { params: { locale }, path } = useRoute('locale')
+const data = await queryContent(path).findOne()
+useSeoMeta({
+  title: data.title,
+})
+
+const createdAt = computed(() => {
+  return Intl.DateTimeFormat(locale, {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }).format(new Date(data.createdAt))
+})
+const modifiedAt = computed(() => {
+  return Intl.DateTimeFormat(locale, {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }).format(new Date(data.modifiedAt))
+})
+</script>
+
+<template>
+  <main>
+    <div class="text-center text-4xl lg:text-3xl font-black pt-8 pb-16">
+      {{ title }}
+    </div>
+    <article
+      data-cursor="text"
+      class="m-auto p-2 sm:px-0 dark:prose-invert prose sm:prose-sm md:prose-md lg:prose-lg"
+    >
+      <h1 class="text-center">
+        {{ data.title }}
+      </h1>
+      <div class="text-sm text-fg-3 text-center">
+        {{ createdAt }} / {{ modifiedAt }}
+      </div>
+      <ContentDoc :head="false" />
+      <div class="mt-32 mx-2 flex gap-4 justify-end">
+        <span v-for="tag in data.tags" :key="tag" class="text-sm text-fg-3 border-fg-3 border px-2 rounded">
+          #{{ tag }}
+        </span>
+      </div>
+      <div class="opacity-75 text-sm font-mono mt-32">
+        <ContentLicense />
+      </div>
+      <div class="font-mono opacity-75 text-sm">
+        <span>$ </span>
+        <NuxtLink
+          :to="`/${locale}`"
+          data-cursor="block"
+        >
+          cd ..
+        </NuxtLink>
+      </div>
+    </article>
+  </main>
+</template>
--- pages/[locale]/notes/[...slug].vue
+++ pages/[locale]/notes/[...slug].vue
@@ -1,32 +1,6 @@
 <script setup lang="ts">
-import 'katex/dist/katex.min.css'
-
-useSeoMeta({
-  title: '1',
-})
-const locale = useRoute('locale').params.locale
 </script>
 
 <template>
-  <main>
-    <h1 class="text-center text-4xl lg:text-6xl font-black pt-8 pb-16">
-      {{ `Jannchie's Notes` }}
-    </h1>
-    <article
-      data-cursor="text"
-      class="m-auto p-2 sm:px-0 dark:prose-invert prose sm:prose-sm md:prose-md lg:prose-lg"
-    >
-      <ContentDoc :head="false" />
-      <ContentLicense />
-      <div class="font-mono opacity-75 text-sm">
-        <span>$ </span>
-        <NuxtLink
-          :to="`/${locale}`"
-          data-cursor="block"
-        >
-          cd ..
-        </NuxtLink>
-      </div>
-    </article>
-  </main>
+  <ArticlePage title="Jannchie's Notes" />
 </template>
