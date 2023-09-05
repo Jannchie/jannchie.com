adf2a613682d62c2ab0bf5f383a8c419a9f9b802
Jannchie <jannchie@gmail.com>
2023-09-06 01:05:44 +0900

    :globe_with_meridians: i18n(en): vzad title

--- i18n/data/en.tsx
+++ i18n/data/en.tsx
@@ -6,7 +6,7 @@ export default {
   'demos': 'Demos',
   'demo-ios-sheet-title': 'Vue.js iOS Sheet',
   'demo-ios-sheet-desc': 'An iOS style Bottom Sheet for the Vue ecosystem, featuring infinite push/pop, smooth scrolling, cool animations, and user-friendly usage.',
-  'demo-vzad-title': 'vzad - Vue.js ズーム＆ドラッグシステム',
+  'demo-vzad-title': 'vzad - Vue.js Zoom & Drag Interaction System',
   'demo-vzad-desc': 'This is a zoom & drag interaction system based on Vue.js. It supports both SVG and HTML modes, and is suitable for creating editors and table games.',
   'demo-gitcm-title': 'Gitcm - Git Commit Message Helper',
   'demo-gitcm-desc': 'Gitcm is a tool for creating Git commit messages. You can quickly create commit messages with a unified format.',
