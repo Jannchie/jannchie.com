bcee78d6b0d3f41f3eba0054252d49b4be4a0c2d
Jianqi Pan <jannchie@gmail.com>
2023-10-05 01:40:33 +0900

    :sparkles: feat(projects): add icon to project cards

--- components/Home/ProjectCard.vue
+++ components/Home/ProjectCard.vue
@@ -1,13 +1,23 @@
-<script setup>
-const { link } = defineProps({
-  title: String,
-  description: String,
-  link: String,
-})
+<script setup lang="ts">
+defineProps<{
+  title: string
+  description: string
+  link: string
+  icon: string
+}>()
 </script>
 
 <template>
-  <NuxtLink data-cursor="block" :href="link" target="_blank" class="p-4 w-72 rounded-xl flex flex-col items-center">
+  <NuxtLink
+    data-cursor="block"
+    :href="link"
+    target="_blank"
+    class="p-4 w-72 rounded-xl flex flex-col items-center"
+  >
+    <i
+      :class="icon"
+      class="opacity-75"
+    />
     <div class="font-bold text-base">
       {{ title }}
     </div>
--- composables/useProjects.ts
+++ composables/useProjects.ts
@@ -3,21 +3,26 @@ export function useProjects() {
     title: 'CodeTime',
     description: 'Programmer Time Tracking.',
     link: 'https://codetime.dev',
+    icon: 'i-tabler-clock',
   }, {
     title: 'Anichart.js',
     description: 'Animate historical data.',
     link: 'https://github.com/Jannchie/anichart.js',
+    icon: 'i-tabler-chart-histogram',
   }, {
     title: 'Zeroroku',
     description: 'Data Observatory.',
     link: 'https://zeroroku.com',
+    icon: 'i-tabler-brand-bilibili',
   }, {
     title: 'Roku UI',
     description: 'React UI Library.',
     link: 'https://roku-ui.vercel.app',
+    icon: 'i-tabler-components',
   }, {
     title: 'Jannchie\'s Blog',
     description: 'Here.',
     link: 'https://github.com/Jannchie/jannchie.com',
+    icon: 'i-tabler-building-cottage',
   }]
 }
