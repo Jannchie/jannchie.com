3ccbaa1dc327e32d1212f33586de7d90112adebd
Jianqi Pan <jannchie@gmail.com>
2023-10-15 18:28:48 +0900

    :adhesive_bandage: fix(view-transition): do not apply view transition on landing

--- composables/useViewTransitionDark.ts
+++ composables/useViewTransitionDark.ts
@@ -2,15 +2,20 @@ export function useViewTransitionDark() {
   const { x, y } = useMouse()
   const isDark = useDark({
     onChanged: (isDark) => {
+      const a = document.documentElement.getAttribute('color-scheme')
+      if (a === 'dark' && isDark) { return }
+      if (a === 'light' && !isDark) { return }
+      if (a === null && !isDark) { return }
       setThemeAttributeWithAnimation(isDark ? 'dark' : 'light', x, y)
     },
   })
-
   function setThemeAttributeWithAnimation(theme: string, x: Ref<number>, y: Ref<number>) {
+    // @ts-expect-error: Transition API
     if (typeof window === 'undefined' || typeof document.startViewTransition === 'undefined') {
       document.documentElement.setAttribute('color-scheme', theme)
       return
     }
+
     const endRadius = Math.hypot(
       Math.max(x.value, innerWidth - x.value),
       Math.max(y.value, innerHeight - y.value),
